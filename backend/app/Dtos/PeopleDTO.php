64865784b5d479ceb0a4a50547f73e7a6f4c5328
Hermes <hermeslmj@gmail.com>
2025-09-09 23:26:24 -0300

    Including the transformation of the return of SWAPI on a DTO. Must be refined later to become cleaner and handle exception

---
+++ backend/app/Dtos/PeopleDTO.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\DTOs;
+
+class PeopleDTO
+{
+
+    public string $uid;
+    public string $name;
+    public string $gender;
+    public string $eyecolor;
+    public string $haircolor;
+    public string $height;
+    public string $mass;
+
+    public array $movies;
+
+    /**
+     * @param string $uid
+     * @param string $name
+     * @param string $gender
+     * @param string $eyecolor
+     * @param string $haircolor
+     * @param string $height
+     * @param string $mass
+     * @param array $movies
+     */
+    public function __construct(
+        string $uid,
+        string $name,
+        string $gender,
+        string $eyecolor,
+        string $haircolor,
+        string $height,
+        string $mass,
+        array $movies
+    ) {
+        $this->uid = $uid;
+        $this->name = $name;
+        $this->gender = $gender;
+        $this->eyecolor = $eyecolor;
+        $this->haircolor = $haircolor;
+        $this->height = $height;
+        $this->mass = $mass;
+        $this->movies = $movies;
+    }
+    
+}
